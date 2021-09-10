b2780a0e54c64d613f9f22d96e1c15a48955fe95
SedlarDavid <sedlardavid.cz@gmail.com>
2021-09-10 13:49:34 +0200

    dashboard activity added, reroute prepared, api class added

--- ak7mt/app/src/main/java/cz/sedlardavid/ak7mt/MainActivity.kt
+++ ak7mt/app/src/main/java/cz/sedlardavid/ak7mt/MainActivity.kt
@@ -1,11 +1,26 @@
 package cz.sedlardavid.ak7mt
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //TODO handle init app state reroute to dashboard
+        // Get location permission
+        //  - Load city from storage ?? continue to app
+        Timer().schedule(5000) {
+            rerouteToDashboard()
+        }
+
+
+    }
+
+    private fun rerouteToDashboard() {
+        startActivity(Intent(this, DashboardActivity::class.java))
     }
 }
