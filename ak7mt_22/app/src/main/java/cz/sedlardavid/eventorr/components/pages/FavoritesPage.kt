5ae8fda4d5abbd553110515cb33ff71e2bc479b5
SedlarDavid <sedlardavid.cz@gmail.com>
2023-01-22 16:30:46 +0100

    search favs via user input

--- ak7mt_22/app/src/main/java/cz/sedlardavid/eventorr/components/pages/FavoritesPage.kt
+++ ak7mt_22/app/src/main/java/cz/sedlardavid/eventorr/components/pages/FavoritesPage.kt
@@ -1,20 +1,24 @@
 package cz.sedlardavid.eventorr.components.pages
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import cz.sedlardavid.eventorr.components.event.EventTile
+import cz.sedlardavid.eventorr.models.EventModel
 import cz.sedlardavid.eventorr.viewModels.EventsViewModel
-
 
 @Composable
 fun FavoritesPage(
@@ -22,8 +26,11 @@ fun FavoritesPage(
 
 ) {
     val viewModel: EventsViewModel = hiltViewModel()
-    val favorites = viewModel.favorites.observeAsState()
 
+    var search by rememberSaveable { mutableStateOf("") }
+
+    var renderFavorites by remember { mutableStateOf(mutableListOf<EventModel>()) }
+    renderFavorites = viewModel.favorites.value!!.toMutableList()
     LazyColumn {
         item {
             Text(
@@ -32,8 +39,49 @@ fun FavoritesPage(
                 fontWeight = FontWeight.Bold
             )
         }
-        items(favorites.value!!.size) { index ->
-            EventTile(favorites.value!![index], navController)
+
+        item {
+
+            TextField(
+                value = search,
+                onValueChange = {
+                    search = it
+                    renderFavorites = if (search.isNotBlank())
+                        viewModel.favorites.value!!.filter { m -> m.event.title.contains(search, ignoreCase = true) }.toMutableList()
+                    else
+                        viewModel.favorites.value!!.toMutableList()
+                },
+                label = { Text("Search") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 20.dp, end = 20.dp, bottom = 20.dp), colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.White, //hide the indicator
+                    unfocusedIndicatorColor = Color.White,
+                    textColor = Color.White,
+                    focusedLabelColor = Color.White,
+                    unfocusedLabelColor = Color.White,
+                    cursorColor = Color.White
+                )
+            )
+
         }
+        if (renderFavorites.isNotEmpty())
+            items(renderFavorites.size) { index ->
+                EventTile(renderFavorites[index], navController)
+            }
+        else
+            item {
+                Text(
+                    text = "No favorites yet!",
+                    color = Color.White,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp, top = 200.dp),
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+            }
     }
 }
