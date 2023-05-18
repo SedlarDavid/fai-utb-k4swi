29bc8eb04b22aa333055c277bbebd5eed714665c
SedlarDavid <sedlar@cngroup.dk>
2023-05-18 15:02:20 +0200

    Annealing base

--- ak8mi_22/semestral_work.py
+++ ak8mi_22/semestral_work.py
@@ -82,3 +82,34 @@ print(best2)
 # ----- Annealing -----
 
 
+for _ in range(countOfRuns):
+
+    pos1 = best1 = np.array(generate_vector())
+    pos2 = best2 = np.array(generate_vector())
+    pos3 = best3 = np.array(generate_vector())
+    
+    num_dimensions = 5  # Number of dimensions in the search space
+    lower_bound = -5.12  # Lower bound of the search space
+    upper_bound = 5.12  # Upper bound of the search space
+    initial_temperature = 100.0  # Initial temperature for annealing
+    cooling_rate = 0.01  # Cooling rate for annealing
+    num_iterations = 1000  # Number of iterations for the simulated annealing
+
+
+    #Run for first dimension
+    for _ in range(D1):
+        for iteration in range(FES):
+
+            #Dejong1
+            temperature = initial_temperature * np.exp(-cooling_rate * iteration)
+
+            candidate_solution =  np.array(generate_vector())
+            candidate_fitness = dejong1(candidate_solution)
+
+            if candidate_fitness < current_fitness or np.random.uniform() < np.exp(-(candidate_fitness - current_fitness) / temperature):
+                current_solution = candidate_solution
+                current_fitness = candidate_fitness
+
+            if candidate_fitness < best_fitness:
+                best_solution = candidate_solution
+                best_fitness = candidate_fitness
