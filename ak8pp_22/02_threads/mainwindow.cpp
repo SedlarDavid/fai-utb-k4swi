64e2cc3ae00fd6b698efbd0f63c46481ec3467c7
SedlarDavid <sedlardavid.cz@gmail.com>
2023-06-04 17:07:53 +0200

    threads logic

--- ak8pp_22/02_threads/mainwindow.cpp
+++ ak8pp_22/02_threads/mainwindow.cpp
@@ -1,15 +1,120 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+Worker::Worker(QObject *parent) : QObject(parent) {}
+
+void Worker::calculateFactorial(int number) {
+    long long result = 1;
+
+    for (int i = 2; i <= number; ++i) {
+        result *= i;
+
+        // Report progress every 10000 iterations or at the end of the loop
+        if (i % 10000 == 0 || i == number) {
+            emit progressChanged(i * 100 / number);
+        }
+
+        if (QThread::currentThread()->isInterruptionRequested()) {
+            emit progressChanged(0); // Reset progress bar
+            return; // Computation canceled
+        }
+    }
+
+    emit progressChanged(0); // Reset progress bar
+    emit resultReady(QString::number(result));
 }
 
-MainWindow::~MainWindow()
-{
+void Worker::calculatePrimes(int range) {
+    QVector<bool> primes(range + 1, true);
+    primes[0] = primes[1] = false;
+
+    for (int p = 2; p * p <= range; ++p) {
+        if (primes[p]) {
+            for (int i = p * p; i <= range; i += p) {
+                primes[i] = false;
+            }
+        }
+
+        if (QThread::currentThread()->isInterruptionRequested()) {
+            emit progressChanged(0); // Reset progress bar
+            return; // Computation canceled
+        }
+    }
+
+    QStringList primeList;
+    for (int i = 0; i <= range; ++i) {
+        if (primes[i]) {
+            primeList.append(QString::number(i));
+        }
+
+        // Report progress every 1000 numbers or at the end of the loop
+        if (i % 1000 == 0 || i == range) {
+            emit progressChanged(i * 100 / range);
+        }
+
+        if (QThread::currentThread()->isInterruptionRequested()) {
+            emit progressChanged(0); // Reset progress bar
+            return; // Computation canceled
+        }
+    }
+
+    emit progressChanged(0); // Reset progress bar
+    emit resultReady(primeList.join(", "));
+}
+
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+
+    workerThread = new QThread(this);
+    worker = new Worker();
+
+    worker->moveToThread(workerThread);
+
+    connect(worker, SIGNAL(resultReady(QString)), this, SLOT(updateResult(QString)));
+    connect(worker, SIGNAL(progressChanged(int)), this, SLOT(updateProgress(int)));
+    connect(workerThread, SIGNAL(finished()), worker, SLOT(deleteLater()));
+    connect(workerThread, SIGNAL(finished()), workerThread, SLOT(deleteLater()));
+
+    // Reset the progress bar to zero on application start
+    ui->progressBar->setValue(0);
+
+    workerThread->start();
+}
+
+
+MainWindow::~MainWindow() {
+    workerThread->quit();
+    workerThread->wait();
     delete ui;
 }
 
+void MainWindow::on_factorialButton_clicked() {
+    int number = ui->numberSpinBox->value();
+    ui->resultLabel->setText("Calculating factorial...");
+
+    QMetaObject::invokeMethod(worker, "calculateFactorial", Qt::QueuedConnection, Q_ARG(int, number));
+}
+
+void MainWindow::on_primeButton_clicked() {
+    int range = ui->rangeSpinBox->value();
+    ui->resultLabel->setText("Calculating primes...");
+
+    QMetaObject::invokeMethod(worker, "calculatePrimes", Qt::QueuedConnection, Q_ARG(int, range));
+}
+
+void MainWindow::on_cancelButton_clicked() {
+    workerThread->requestInterruption();
+    workerThread->quit();
+    workerThread->wait();
+    ui->resultLabel->setText("Calculation canceled.");
+}
+
+void MainWindow::updateResult(QString result) {
+    ui->resultLabel->setText(result);
+}
+
+void MainWindow::updateProgress(int value) {
+    ui->progressBar->setValue(value);
+}
--- ak8pp_22/02_threads/mainwindow.h
+++ ak8pp_22/02_threads/mainwindow.h
@@ -2,20 +2,45 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QThread>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
-class MainWindow : public QMainWindow
-{
+class Worker : public QObject {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit Worker(QObject *parent = nullptr);
+
+public slots:
+    void calculateFactorial(int number);
+    void calculatePrimes(int range);
+
+signals:
+    void resultReady(QString result);
+    void progressChanged(int value);
+};
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_factorialButton_clicked();
+    void on_primeButton_clicked();
+    void on_cancelButton_clicked();
+    void updateResult(QString result);
+    void updateProgress(int value);
 
 private:
     Ui::MainWindow *ui;
+    QThread *workerThread;
+    Worker *worker;
 };
+
 #endif // MAINWINDOW_H
