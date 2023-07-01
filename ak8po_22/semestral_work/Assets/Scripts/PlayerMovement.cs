b56310b1a3cfb446bbb67374f65102d8c2816425
SedlarDavid <sedlar@cngroup.dk>
2023-07-01 13:15:23 +0200

    player animations

--- ak8po_22/semestral_work/Assets/Scripts/PlayerMovement.cs
+++ ak8po_22/semestral_work/Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _body;
     [SerializeField] private float speed;
 
+    private Animator _anim;
+
+    private bool _grounded;
+
     private void Awake()
     {
         _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -28,9 +33,27 @@ public class PlayerMovement : MonoBehaviour
             _ => scaleTransform.localScale
         };
 
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) & _grounded)
         {
-            _body.velocity = new Vector2(_body.velocity.x, speed);
+            Jump();
+        }
+
+        _anim.SetBool("run", horizontalInput != 0);
+        _anim.SetBool("grounded", _grounded);
+    }
+
+    private void Jump()
+    {
+        _body.velocity = new Vector2(_body.velocity.x, speed * 0.75f);
+        _anim.SetTrigger("jump");
+        _grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            _grounded = true;
         }
     }
 }
